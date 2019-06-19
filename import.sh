92b47632c13fcfc0375fcb5f4e9add52fe9e01ef
Anderson Correa <anderson.correa@accenture.com>
2019-06-19 16:45:37 -0300

    removendo do script a pasta .git e os arquivos README.md e import.sh

--- import.sh
+++ import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find . -type d | sed "s|^.\{1\}|${HOME}|g" | xargs mkdir -p
+find . \( -path ./.git -o -iname README.md -o -iname import.sh \) -prune -o -type d -print | sed "s|^.\{1\}|${HOME}|g" | xargs mkdir -p
 
-find . -type f -print0 |
+find . \( -path ./.git -o -iname README.md -o -iname import.sh \) -prune -o -type f -print0 |
   while IFS= read -r -d '' line; do
     origin=$(echo ${line} | sed "s|^.\{1\}|$(pwd)|g")
     destination=$(echo ${line} | sed "s|^.\{1\}|${HOME}|g")
