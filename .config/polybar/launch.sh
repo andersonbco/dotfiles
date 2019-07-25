4f88895d584b93f133a0129cc9d20d9dead3b959
Anderson Correa <anderson.correa@accenture.com>
2019-07-24 23:35:49 -0300

    ajustando os scripts do xrandr

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+. $HOME/dotfiles/xrandr_utils
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-hdmi_off=$(xrandr -q | grep "HDMI-1" | grep "disconnected")
+count_monitors
 
-if [ -n "$hdmi_off" ]
+if [[ $NUMBER_OF_MONITORS -eq 1 ]]
 then
-	MONITOR=eDP-1 polybar single_bar &
+  get_default_monitor
+	MONITOR=$DEFAULT_MONITOR polybar single_bar &
 else
-  MONITOR=HDMI-1 polybar main_bar &
-	MONITOR=eDP-1 polybar aux_bar &
+  get_multiple_monitors
+  MONITOR=${MONITOR_LIST[0]} polybar main_bar &
+	MONITOR=${MONITOR_LIST[1]} polybar aux_bar &
 fi
 
 
--- .screenlayout/launch.sh
+++ .screenlayout/launch.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
- 
+
 . $HOME/dotfiles/xrandr_utils
 
-count_monitors 
+count_monitors
 
 if [[ $NUMBER_OF_MONITORS -eq 1 ]]
 then
   get_default_monitor
-  echo "default"
   ~/.screenlayout/default.sh $DEFAULT_MONITOR
 else
   get_multiple_monitors
-  echo "dual"
   ~/.screenlayout/dual_screen.sh ${MONITOR_LIST[0]} ${MONITOR_LIST[1]}
 fi
