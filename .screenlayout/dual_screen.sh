19edbc5207c1d24b72483f5fb4f8af64eac48da4
Anderson Correa <anderson.correa@accenture.com>
2019-09-05 15:18:33 -0300

    removendo os scripts de screenlayout / passa a usar o xrandr

--- .screenlayout/dual_screen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-. $HOME/dotfiles/xrandr_utils
-
-get_monitors
-
-xrandr --output ${MONITOR_LIST[0]} --mode 1920x1080 --pos 0x0 --rotate normal --output ${MONITOR_LIST[1]} --primary --mode 1920x1080 --pos 1920x0 --rotate normal
